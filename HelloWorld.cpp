3db0a1a73491f3d02d1d78e8d23a1c8f393fe5c9
micah <micahbhawkins@gmail.com>
2017-10-15 23:42:43 -0700

    the skull is move-y with arrows and mouse/space

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,15 +4,22 @@
 
 int main(int argc, char** argv) {
   sf::RenderWindow renderWindow(sf::VideoMode(1920, 1080), "Hello, funkjin");
+  sf::Texture texture;
+  if (!texture.loadFromFile("skull.png")) {
+    renderWindow.close();
+  }
+
+  sf::Sprite sprite(texture);
+  sprite.setPosition(0,0);
 
   // Create a font object and load it from file relative
   sf::Font font;
-  if (!font.loadFromFile("Georgia.ttf")) {
+  if (!font.loadFromFile("AnonymousPro-Regular.ttf")) {
     return 42; // Don't Panic
   }
 
   // Create a Hello funkjin text object using our font and specifying a size
-  sf::Text text("Hello, funkjin :)", font, 128);
+  sf::Text text("hi funkjin", font, 128);
 
   // Set the color to green
   text.setColor(sf::Color::Green);
@@ -28,18 +35,34 @@ int main(int argc, char** argv) {
 
 
 
-  while (renderWindow.isOpen()){
+  while (renderWindow.isOpen()) {
     sf::Event event;
 
-    while (renderWindow.pollEvent(event)){
-      if (event.type == sf::Event::Closed)
-        {
+    while (renderWindow.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
           renderWindow.close();
-        }
+      }
+
+      if (event.type == sf::Event::KeyPressed) {
+        // Event driven input handling
+        if (event.key.code == sf::Keyboard::Left)
+          sprite.move(-5, 0);
+        if (event.key.code == sf::Keyboard::Right)
+          sprite.move(5, 0);
+        if (event.key.code == sf::Keyboard::Up)
+          sprite.move(0, -5);
+        if (event.key.code == sf::Keyboard::Down)
+          sprite.move(0, 5);
+      }
     }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+      sprite.setPosition(static_cast<sf::Vector2f>(sf::Mouse::getPosition(renderWindow)));
+
     renderWindow.clear(sf::Color::White);
     // Draw our text object
     renderWindow.draw(text);
+    renderWindow.draw(sprite);
     renderWindow.display();
   }
 }
